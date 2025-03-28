77497ac7912bde700f7b78e250af05ad2dbda653
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-03-28 14:38:01 +0530

    Create textdata.js

---
+++ textdata.js
@@ -0,0 +1,14 @@
+{
+"footer": "> ​🇵​​🇴​​🇼​​🇪​​🇷​​🇩​ ​🇧​​🇾​ ​🇵​​🇦​​🇬​​🇳​​🇬​​🇳​​🇦​​🇸​​🇮​​🇷​​🇮​ ​🇹​​🇭​​🇪​​🇷​​🇴​",
+"mainimg": "https://i.ibb.co/VpV6vqGV/toolzin-com-94752677804.jpg",
+"replyMsg": {
+  "givetext": "*Please give me text..!✏️*",
+  "giveurl": "*Please give me Url..!🖇️*",
+  "cowner": "*Owner Only Comade..!🪄*",
+  "gadm": "*Give Me Group Admin..!🫣*",
+  "gonly": "*This Comade Only Group..!👥*",
+  "pinbox": "*This Comade Only Use Inbox..!👁️*",
+  "emsg": "*Erorr..!*"
+
+}
+}
